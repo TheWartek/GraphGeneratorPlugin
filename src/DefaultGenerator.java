356fe4acd27d0bb72fdfa444bdd16961ce2b003f
TheWartek <grief888@gmail.com>
2012-05-10 01:17:45 +0200

    Dodanie klasy generatora

---
+++ src/DefaultGenerator.java
@@ -0,0 +1,20 @@
+
+import pl.mgrproject.api.Graph;
+import pl.mgrproject.api.plugins.Generator;
+
+
+public class DefaultGenerator implements Generator {
+
+    @Override
+    public String getName() {
+	
+	return null;
+    }
+
+    @Override
+    public Graph<?> getGraph(int arg0) {
+	
+	return null;
+    }
+
+}
