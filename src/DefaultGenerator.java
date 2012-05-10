7615af74b101b85c7a420be30494f5ace6440e40
TheWartek <grief888@gmail.com>
2012-05-10 16:44:38 +0200

    Po dluzszym czasie szukania znalazlem ten dosc glupi blad. Zamiast liczb losowych jako wspolrzednych wierzcholka przekazywane byly wymiary panelu rysujacego...

--- src/DefaultGenerator.java
+++ src/DefaultGenerator.java
@@ -2,6 +2,7 @@
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import pl.mgrproject.api.Environment;
 import pl.mgrproject.api.Graph;
@@ -18,9 +19,10 @@ public class DefaultGenerator implements Generator {
     @Override
     public Graph<?> getGraph(int n) {
 	Graph<Integer> g = new Graph<Integer>(n);
+	Random r = new Random();
 	
 	for (int i = 0; i < n; ++i) {
-	    g.addVertex(new Point(Environment.getGraphPanelWidth(), Environment.getGraphPanelHeight()));
+	    g.addVertex(new Point(r.nextInt(Environment.getGraphPanelWidth()), r.nextInt(Environment.getGraphPanelHeight())));
 	}
 	
 	List<Point> vertices = g.getVertices();
